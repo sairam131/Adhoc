cc2ce0befded6cce2d684bef862c335203a9d48f
dr aditya <dr aditya@LAPTOP-DEDAPJLO.bbrouter>
2021-05-04 21:33:51 +0530

    commit it

--- School/src/pk1/StudentNames.java
+++ School/src/pk1/StudentNames.java
@@ -5,6 +5,7 @@ public class StudentNames {
 	public static void main(String[] args) {
 		System.out.println("student name is Dhoni");
 		System.out.println("sankdcjksdnvjcmakjvmskc");
+		System.out.println("1234567");
 		
 	}
 
